33975b7392bd7853e74db60cf1d3b2b32826c9e6
Matt Reiferson <mreiferson@gmail.com>
2010-09-07 11:34:52 -0400

    returning bool, renamed method

--- wkhtmltox.pyx
+++ wkhtmltox.pyx
@@ -1,5 +1,5 @@
 cdef extern from "wkhtmltox/pdf.h":
-    int wkhtmltopdf_init(int use_graphics)
+    bint wkhtmltopdf_init(int use_graphics)
 
-def my_pdf_init(int use_graphics):
+def pdf_init(int use_graphics):
     return wkhtmltopdf_init(use_graphics)
