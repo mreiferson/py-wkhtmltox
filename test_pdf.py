10598add69d9aa1a3b1206ea501302d634415176
Matt Reiferson <mreiferson@gmail.com>
2010-09-11 16:07:04 -0400

    fixed object setting typo (should be 'page' not 'path')

--- test_pdf.py
+++ test_pdf.py
@@ -2,5 +2,5 @@ import wkhtmltox
 
 pdf = wkhtmltox.Pdf()
 pdf.set_global_setting('out', 'test.pdf')
-pdf.set_object_setting('path', 'http://www.google.com')
+pdf.set_object_setting('page', 'http://www.google.com')
 pdf.convert()
