43d2d1b288c57140cb4fdb9a1848d77af77cb4bf
Michal Plichta <michalplichta@scurri.com>
2022-04-26 16:30:08 +0200

    TP-26791 Python 3 upgrade py-wkhtmltox upgrade

--- tests/test_wkhtmltox.py
+++ tests/test_wkhtmltox.py
@@ -10,11 +10,11 @@ def test_image():
     img.set_global_setting(b'in', b'google.html')
     img.convert()
     assert os.path.isfile(test_filename)
-    with open('test_org.jpg') as test_org, open(test_filename) as new_jpg:
-        original = test_org.read()
-        generated = new_jpg.read()
-    assert original == generated
+    with open('test_org.jpg', 'rb') as org, open(test_filename, 'rb') as new:
+        original = org.read().decode('ISO-8859-1')
+        generated = new.read().decode('ISO-8859-1')
     os.remove(test_filename)
+    assert original == generated
 
 
 def test_pdf():
@@ -26,9 +26,10 @@ def test_pdf():
     pdf.convert()
 
     assert os.path.isfile(test_filename)
-    with open('test_org.pdf') as test_org, open(test_filename) as new_pdf:
-        original = test_org.read()
-        generated = re.sub(r'CreationDate \(D:\d{14}\+',
-                           'CreationDate (D:20220425172518+', new_pdf.read())
-    assert original == generated
+    with open('test_org.pdf', 'rb') as org, open(test_filename, 'rb') as new:
+        original = org.read().decode('ISO-8859-1')
+        generated = re.sub(r"CreationDate \(D:\d{14}\+\d{2}'00",
+                           "CreationDate (D:20220426161454+02'00",
+                           new.read().decode('ISO-8859-1'))
     os.remove(test_filename)
+    assert original == generated
